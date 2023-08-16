8b2018b4f66cbabafabd8927447d6d39172bd5ba
ryosuke-betw <sir.kasama1218@icloud.com>
2023-08-16 11:57:10 +0900

    Django演習-様々なビューの作成 Django演習3のプルリクエストを上げさせて頂きます。
    本章では
    ・"index" ページ
    ・"detail" ページ
    ・"result" ページ
    ・"vote"ページ
    ・例外処理
    をDjangoで作成しました。
    
    今回確認していただきたい内容は二点あります。
    
    (i) indexページが表示されること
    
    (ii) detailページに飛んだ際、404エラーが発生すること(与える数を引数にとるオブジェクトが存在しないため)
    
    確認していただく手順は以下の通りです。
    
    1）python manage.py runserver をターミナルで入力し、サーバーを起動
    
    2）＜(i)の確認＞ "http://127.0.0.1:8000/polls/"を開き、箇条書きの質問のリストが表示されていることを確認（"・What's up?"という項目）
    
    3）＜(ii)の確認＞
    "http://127.0.0.1:8000/polls/34"を開き、404のエラーが表示されている事を確認（No Question matches the given query.というもの）
    
    以上の手順でご確認をお願いいたします。

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7i6)=s$22banskkc1y%6^2hq)o@d-l&a&#&io6*sps_prwlz8+'
+SECRET_KEY = 'django-insecure-$y2&1n28zgyo8&7wp3$ls1*fwulgen($kx*g%5^0lj%a=4wkif'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
